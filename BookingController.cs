2aeeab91dc9fcc67607c5adde4ead4440e850a2e
sajjad@gs-ms.com <sajjad@gs-ms.com>
2023-09-25 10:25:44 +0530

    Fist Commit - Routing / Controllers

---
+++ BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelWebApp
+{
+    public class BookingController : Controller
+    {
+        public IActionResult BookingsList()
+        {
+            return View("Bookings");
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelWebApp.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }  = string.Empty;
+        public string CustomerEmail { get; set; } = string.Empty;
+        public string CustomerPhone { get; set; } = string.Empty;
+        public string CustomerCity { get; set; } = string.Empty;
+    }
+}
